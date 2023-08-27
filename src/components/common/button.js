7c3ef811e1ae847ebd4ed9173b178c4e4915d324
younggyo <lee4519698@hanmail.net>
2023-08-28 00:16:44 +0900

    Feat : Component Button

---
+++ src/components/common/button.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from 'styled-components';
+import palette from '../../styles/palette';
+
+const StyledButton = styled.button`
+  border: none;
+  border-radius: 4px;
+  font-size: 1rem;
+  font-wight: bold;
+  padding: 0.25rem 1rem;
+  color: black;
+  outline: none;
+  cursor: pointer;
+
+  background: ${palette.yellow[0]};
+`;
+
+const button = (props) => <StyledButton {...props} />;
+
+export default button;
