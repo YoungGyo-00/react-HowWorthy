408e972aeeef2e410fd1e142119cbdd36e541314
younggyo <lee4519698@hanmail.net>
2023-08-28 00:16:27 +0900

    Feat : Page Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import PostPage from './pages/PostPage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import RatingPage from './pages/RatingPage';
 
 const App = () => {
-  return <div>Hello</div>;
+  return (
+    <Routes>
+      <Route Component={PostPage} path="/post" />
+      <Route Component={LoginPage} path="/" />
+      <Route Component={RegisterPage} path="/register" />
+      <Route Component={RatingPage} path="/rating" />
+    </Routes>
+  );
 };
 
 export default App;
---
+++ src/pages/RatingPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const RatingPage = () => {
+  return <div>인스타 별점 페이지</div>;
+};
+
+export default RatingPage;
