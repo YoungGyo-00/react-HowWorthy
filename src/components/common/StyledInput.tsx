a9e9a613a24547dc7d68d6c3feb32bedd84865d1
younggyo <lee4519698@hanmail.net>
2023-08-31 16:04:57 +0900

    Feat : StyledComponent 분리

---
+++ src/components/common/StyledInput.tsx
@@ -0,0 +1,35 @@
+import palette from 'src/utils/palette';
+import { styled } from 'styled-components';
+
+const Title = styled.div`
+  margin: 0;
+  color: ${palette.black[0]};
+  width: fit-content;
+  margin-bottom: 5px;
+  margin-left: 1vh;
+`;
+
+const Input = styled.input`
+  font-size: 1rem;
+  border-radius: 10px;
+  width: 95%;
+  height: 1.5rem;
+  margin-bottom: 1rem;
+`;
+
+const StyledInput = ({
+  title,
+  name,
+  placeholder,
+}: {
+  title: string;
+  name: string;
+  placeholder: string;
+}) => {
+  <>
+    <Title>{title}</Title>
+    <Input name={name} placeholder={placeholder} />
+  </>;
+};
+
+export default StyledInput;
