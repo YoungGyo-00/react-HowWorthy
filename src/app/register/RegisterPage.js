3cd61e6b6a472cec3530fd9ce39accecacc7a641
younggyo <lee4519698@hanmail.net>
2023-09-02 23:42:23 +0900

    Feat : RegisterForm 적용

--- src/app/register/RegisterPage.js
+++ src/app/register/RegisterPage.js
@@ -5,16 +5,43 @@ import palette from 'src/utils/palette';
 import { PageTemplateBlock } from 'src/components/common/PageTemplateBlock';
 
 const Header = styled.div`
-  width: 100;
-  height: 10vh;
+  width: 100%;
+  height: 85px;
   text-align: left;
   background-color: ${palette.yellow[1]};
+  font-size: 50px;
+  font-weight: normal;
+  padding-left: 2rem;
+  padding-top: 1rem;
+  color: ${palette.gray[2]};
+`;
+
+const HeaderImage = styled.img`
+  height: 50px;
+  width: 50px;
+`;
+
+const Title = styled.div`
+  margin-top: 3rem;
+  margin-left: 4rem;
+  font-weight: 600;
+  font-size: 60px;
+`;
+
+const Content = styled.div`
+  margin-left: 4rem;
+  font-weight: 500;
+  font-size: 40px;
 `;
 
 const RegisterPage = () => {
   return (
-    <PageTemplateBlock flex_direction="column">
-      <Header />
+    <PageTemplateBlock flex_direction="column" justify_content="flex-start">
+      <Header>
+        <div>How Worthy</div>
+      </Header>
+      <Title>Welcome to How Worthy!</Title>
+      <Content>Create Account</Content>
       <RegisterForm />
     </PageTemplateBlock>
   );
