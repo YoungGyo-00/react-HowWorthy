13cccfb82c5b6bee439c17a589e0322baaf0c15b
younggyo <lee4519698@hanmail.net>
2023-09-03 16:51:45 +0900

    Feat : Button Switch 적용

--- src/components/common/button.js
+++ src/components/common/button.js
@@ -3,35 +3,59 @@ import styled, { css } from 'styled-components';
 import palette from 'src/utils/palette';
 
 const StyledButton = styled.button`
-  border: none;
-  border-radius: 4px;
-  font-size: 1.5rem;
-  font-weight: bold;
-  padding: 0.25rem 1rem; // 위아래, 왼쪽오른쪽
-  color: black;
-  outline: none;
-  cursor: pointer;
-  border-radius: 10px;
+  ${(props) => {
+    switch (props.color) {
+      case 'gray':
+        return css`
+          border: none;
+          border-radius: 1.7rem;
+          font-size: 2rem;
+          font-weight: 400;
+          padding: 0.7rem 2rem; // 위아래, 왼쪽오른쪽
+          color: ${palette.white[0]};
+          outline: none;
+          cursor: pointer;
+          margin-top: ${props.margin_top};
+          margin-left: ${props.margin_left};
+          position: absolute;
 
-  background-color: ${palette.yellow[0]};
-  &:hover {
-    background-color: ${palette.gray[0]};
-  }
+          background-color: ${palette.gray[0]};
+          &:hover {
+            background-color: ${palette.yellow[0]};
+          }
+        `;
 
-  ${(props) =>
-    props.gray &&
-    css`
-      background-color: ${palette.gray[0]};
-      &:hover {
-        background-color: ${palette.yellow[0]};
-      }
-    `}
+      case 'none':
+        return css`
+          font-size: 1.5rem;
+          font-weight: 400;
+          color: ${palette.gray[0]};
+          outline: none;
+          cursor: pointer;
+          margin-top: ${props.margin_top};
+          margin-left: ${props.margin_left};
+          position: absolute;
+        `;
 
-  ${(props) =>
-    props.margin_top &&
-    css`
-      margin-top: ${props.margin_top};
-    `}
+      default:
+        return css`
+          border: none;
+          font-size: 1.5rem;
+          font-weight: bold;
+          padding: 0.25rem 1rem; // 위아래, 왼쪽오른쪽
+          color: black;
+          outline: none;
+          cursor: pointer;
+          border-radius: 10px;
+          margin-top: ${props.margin_top};
+
+          background-color: ${palette.yellow[0]};
+          &:hover {
+            background-color: ${palette.gray[0]};
+          }
+        `;
+    }
+  }}
 `;
 
 const Button = (props) => <StyledButton {...props} />;
